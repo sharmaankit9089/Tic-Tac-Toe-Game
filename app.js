94b33b9f75e5f04efdd97e509d7a1df3e705e74f
sharmaankit9089 <sharmaankit9089@gmail.com>
2025-12-09 06:03:13 +0530

    Fix win patterns and improve game logic

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ let turnO = true; // Player X, Player O
 //Patterns to Win
 const winPatterns = [
     [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
     [0, 3, 6],
     [0, 4, 8],
     [1, 4, 7],
     [2, 5, 8],
     [2, 4, 6],
-    [3, 4, 5],
-    [6, 7, 8],
 ];
 
 const resetGame = () => {
@@ -75,3 +75,4 @@ const checkWinner = () => {
 newGameBtn.addEventListener("click", resetGame);
 
 resetBtn.addEventListener("click", resetGame);
+
